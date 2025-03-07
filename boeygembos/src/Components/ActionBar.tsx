3be8d9a638d2d25ad12289e243df1c2c3bdf5298
svb <svb@example.com>
2025-03-07 07:51:37 +0000

    init app

---
+++ boeygembos/src/Components/ActionBar.tsx
@@ -0,0 +1,15 @@
+function ActionBar() {
+    return (
+        <>
+            <section>
+
+            </section>
+            <section>
+                Search
+                <input type="text" name="searchbar" id="searchbar" />
+            </section>
+        </>
+    );
+}
+
+export default ActionBar;
---
+++ boeygembos/src/Components/NavBar.tsx
@@ -0,0 +1,14 @@
+function NavBar() {
+    return (
+        <>
+            <menu>
+                <li>Home</li>
+                <li>Boeygem Bos</li>
+                <li>Gallerij</li>
+                <li>Contact</li>
+            </menu>
+        </>
+    );
+}
+
+export default NavBar;

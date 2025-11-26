901318ea4b38f834079e3c2147d7941220aeefba
Your Name <you@example.com>
2025-11-26 14:12:23 +0100

    decap cms

--- tina/__generated__/client.d.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ cacheDir: '/home/syndias/Projects/TnT/boeygembos.github.io/tina/__generated__/.cache/1764149081183', url: 'https://content.tinajs.io/1.6/content/a14e4240-dc11-49b7-80f9-067ca5658a6b/github/release', token: 'f428d04d70f344f5c2e37b26bed58aa861143834', queries,  });
-export default client;
-  
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,24 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    {
+      name: 'decap-cms-admin',
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          if (req.url === '/admin') {
+            // Redirect /admin to /admin/ so the browser treats it as a directory
+            res.writeHead(301, { Location: '/admin/' })
+            res.end()
+            return
+          }
+          if (req.url === '/admin/') {
+            req.url = '/admin/index.html'
+          }
+          next()
+        })
+      }
+    }
+  ]
 })
